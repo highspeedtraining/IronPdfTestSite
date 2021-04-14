8aca8117f64c6c6f47ffd9ee48f55a4979f135c3
laurence.woodward <laurence.woodward@highspeedtraining.co.uk>
2021-04-14 09:56:06 +0100

    Initial Commit

---
+++ IronPdfTestSite/Default.aspx.cs
@@ -0,0 +1,27 @@
+﻿using IronPdf;
+using System;
+using System.Web.UI;
+
+namespace IronPdfTestSite
+{
+	public partial class _Default : Page
+	{
+		protected void Page_Load(object sender, EventArgs e)
+		{
+
+		}
+
+		protected void lb_getPDF_OnClick(object sender, EventArgs e)
+		{
+			var htmlToPdf = new HtmlToPdf();
+
+			var pdfDocument = htmlToPdf.RenderHtmlAsPdf("<html><body><div>Hello World</div></body></html>");
+			var pdfDocumentBinaryData = pdfDocument.BinaryData;
+
+			Response.Clear();
+			Response.AddHeader("Content-Type", "binary/octet-stream");
+			Response.AddHeader("Content-Disposition", $"attachment; filename=\"test.pdf\"; size={pdfDocumentBinaryData.Length}");
+			Response.BinaryWrite(pdfDocumentBinaryData);
+		}
+	}
+}
